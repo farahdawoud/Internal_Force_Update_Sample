b8ad31081973c6eb9cbeb3fcd378d9baaa34e2fd
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-08 14:00:21 +0400

    Create Update Modal with its contents

--- src/screens/HomeScreen/HomeScreen.view.tsx
+++ src/screens/HomeScreen/HomeScreen.view.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import UpdateModal from '../../ui/components/UpdateModal/UpdateModal.view';
 import HomeScreenStyles from './HomeScreen.styles';
 
-export const HomeScreen = (): JSX.Element => {
+export const HomeScreenView = ({
+  isModalVisible,
+  updateData,
+  setModalVisibility,
+}: {
+  isModalVisible: boolean;
+  updateData: any;
+  setModalVisibility: Function;
+}): JSX.Element => {
   return (
     <View style={HomeScreenStyles.mainContainer}>
       <Text>Home Screen</Text>
+      {isModalVisible && (
+        <UpdateModal
+          isModalVisible={isModalVisible}
+          updateData={updateData}
+          setModalVisibility={setModalVisibility}
+        />
+      )}
     </View>
   );
 };
 
-export default HomeScreen;
+export default HomeScreenView;
